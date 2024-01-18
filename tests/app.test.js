641d3a40cb7dbf1881f39383653467a85a48b650
Kevin Morainville <slupshi@gmail.com>
2024-01-18 13:35:53 +0100

    feat: test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,12 +1,13 @@
 import request from 'supertest';
 import app from '../app.js'
 
-describe('GET /post', function() {
-    it('responds with json', function(done) {
+describe('GET /post', function () {
+    it('responds with json', function (done) {
         request(app)
             .get('/post')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect(200, done);
+            .expect(200);
+        done();
     });
 });
